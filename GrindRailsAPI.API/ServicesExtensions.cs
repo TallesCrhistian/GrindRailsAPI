4a63e9fd0b60ef92b5ea426ddaadb98b898385f4
Talles Arriel <tallescrhistiandev@gmail.com>
2023-07-18 22:05:25 -0300

    feat: add configuration inital

---
+++ GrindRailsAPI.API/ServicesExtensions.cs
@@ -0,0 +1,43 @@
+﻿using GrindRailsAPI.Data.Context;
+using GrindRailsAPI.Data.Interfaces;
+using GrindRailsAPI.Data.WorkUnit;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrindRailsAPI.API
+{
+    public static class ServicesExtensions
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration iConfiguration)
+        {
+            services.AddDbContext<AppDbContext>(x => x.UseNpgsql(iConfiguration.GetConnectionString("DefaultConnection"))
+                .EnableSensitiveDataLogging()
+        .LogTo(Console.WriteLine, LogLevel.Information));
+
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddBusiness(this IServiceCollection services)
+        {
+
+            return services;
+        }
+
+        public static IServiceCollection AddRepository(this IServiceCollection services)
+        {
+
+            return services;
+        }
+
+        public static IServiceCollection WorkUnit(this IServiceCollection services)
+        {
+            services.AddScoped<IWorkUnit, WorkUnit>();
+            return services;
+        }
+    }
+}
+
---
+++ GrindRailsAPI.Data/WorkUnit/WorkUnit.cs
@@ -0,0 +1,36 @@
+﻿using GrindRailsAPI.Data.Context;
+using GrindRailsAPI.Data.Interfaces;
+
+namespace GrindRailsAPI.Data.WorkUnit
+{
+    public class WorkUnit : IWorkUnit
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public WorkUnit(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            _appDbContext.Database.BeginTransaction();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _appDbContext.Database.CommitTransactionAsync();
+        }
+
+        public void Rollback()
+        {
+            _appDbContext.Database.RollbackTransaction();
+        }
+
+        public async Task DeleteAsync()
+        {
+            await _appDbContext.Database.EnsureDeletedAsync();
+        }
+    }
+}
